a83a9bd2e540924c82a518f95def3ece54626f1e
lindstrom.elle@gmail.com <lindstrom.elle@gmail.com>
2024-06-25 21:23:35 +0200

    created animation on contact page, created contact-form-server and netlify.toml file

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,30 +1,86 @@
+//import { useEffect } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import styles from '../scss/ContactPage.module.scss';
+import axios from 'axios';
 
-function ContactPage() {
+  interface FormData {
+    name: string;
+    email: string;
+    message: string;
+  }
+  
+  const ContactPage: React.FC = () => {
+    const [formData, setFormData] = useState<FormData>({
+      name: '',
+      email: '',
+      message: ''
+    });
+  
+    const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+      const { name, value } = e.target;
+      setFormData((prevData) => ({ ...prevData, [name]: value }));
+    };
+  
+    const handleSubmit = async (e: FormEvent): Promise<void> => {
+      e.preventDefault();
+      try {
+        const response = await axios.post('http://localhost:3000/send', formData);
+        if (response.status === 200) {
+          alert('Message sent successfully');
+        }
+      } catch (error) {
+        alert('Failed to send message');
+      }
+    };
+  
+
   return (
     <>
     <div className={styles['contact-page-text']}>
     <h2 className={styles['contact-page-title']}>
         CONTACT</h2>
+        <div className={styles['typing-container']}>
         <p className={styles['contact-page-subtitle']}>
         Reach out :)
   </p>
   </div>
+  </div>
   <div className={styles.contactContent}>
-        <form className={styles.contactForm}>
-          <label htmlFor="name">Name*</label>
-          <input type="text" id="name" name="name" /* placeholder="Olivia" */ />
+        <form className={styles.contactForm} onSubmit={handleSubmit}>
+        <label htmlFor="name">Name*</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
           
           <label htmlFor="email">Email address*</label>
-          <input type="email" id="email" name="email" /* placeholder="hello@reallygreatsite.com" */ />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
           
           <label htmlFor="message">Message*</label>
-          <textarea id="message" name="message" /* placeholder="Type your message here" */></textarea>
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            required
+          ></textarea>
           
           <div className={styles.buttonContainer}>
             <button type="submit">SUBMIT</button>
           </div>
         </form>
+        
         <div className={styles.internshipDetails}>
           <h2>INTERNSHIP</h2>
           <p>My primary goal is to find an environment where I can enhance my skills as a developer and make meaningful contributions. I am dedicated to learning and growing throughout this experience.</p>
