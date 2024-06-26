2ca3c62b18adc30d97018842a87b369cecbafbb2
lindstrom.elle@gmail.com <lindstrom.elle@gmail.com>
2024-06-26 11:18:09 +0200

    Changed contact form to contain contact info directly, removed server side, created and added footer + styles.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+
+import styles from '../scss/Footer.module.scss';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className={styles.footer}>
+      ©2024 Ellinor Lindström
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,96 +1,51 @@
-//import { useEffect } from 'react';
-import { ChangeEvent, FormEvent, useState } from 'react';
+import Footer from '../components/Footer';
 import styles from '../scss/ContactPage.module.scss';
-import axios from 'axios';
 
-  interface FormData {
-    name: string;
-    email: string;
-    message: string;
-  }
-  
-  const ContactPage: React.FC = () => {
-    const [formData, setFormData] = useState<FormData>({
-      name: '',
-      email: '',
-      message: ''
-    });
-  
-    const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
-      const { name, value } = e.target;
-      setFormData((prevData) => ({ ...prevData, [name]: value }));
-    };
-  
-    const handleSubmit = async (e: FormEvent): Promise<void> => {
-      e.preventDefault();
-      try {
-        const response = await axios.post('http://localhost:3000/send', formData);
-        if (response.status === 200) {
-          alert('Message sent successfully');
-        }
-      } catch (error) {
-        alert('Failed to send message');
-      }
-    };
-  
-
+const ContactPage: React.FC = () => {
   return (
     <>
-    <div className={styles['contact-page-text']}>
-    <h2 className={styles['contact-page-title']}>
-        CONTACT</h2>
+      <div className={styles['contact-page-text']}>
+        <h2 className={styles['contact-page-title']}>
+          CONTACT
+        </h2>
         <div className={styles['typing-container']}>
-        <p className={styles['contact-page-subtitle']}>
-        Reach out :)
-  </p>
-  </div>
-  </div>
-  <div className={styles.contactContent}>
-        <form className={styles.contactForm} onSubmit={handleSubmit}>
-        <label htmlFor="name">Name*</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
-          
-          <label htmlFor="email">Email address*</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-          
-          <label htmlFor="message">Message*</label>
-          <textarea
-            id="message"
-            name="message"
-            value={formData.message}
-            onChange={handleChange}
-            required
-          ></textarea>
-          
-          <div className={styles.buttonContainer}>
-            <button type="submit">SUBMIT</button>
+          <p className={styles['contact-page-subtitle']}>
+            Reach out :)
+          </p>
+        </div>
+      </div>
+      <div className={styles.contactContent}>
+        <div className={styles.contactDetailsContainer}>
+          <div className={styles['contact-details']}>
+            <span className={styles['contact-title']}>EMAIL:</span>
+            <a href="mailto:lindstrom.elle@gmail.com" className={styles['contact-address']}>
+            &#x2197; lindstrom.elle@gmail.com
+            </a>
           </div>
-        </form>
-        
+          <div className={styles['contact-details']}>
+            <span className={styles['contact-title']}>LINKEDIN:</span>
+            <a href="https://www.linkedin.com/in/ellinor-lindstrom" className={styles['contact-address']}>
+            &#x2197; Ellinor Lindström
+            </a>
+          </div>
+          <div className={styles['contact-details']}>
+            <span className={styles['contact-title']}>GITHUB:</span>
+            <a href="https://github.com/ellinorlindstrom" className={styles['contact-address']}>
+            &#x2197; ellinorlindstrom
+            </a>
+          </div>
+        </div>
         <div className={styles.internshipDetails}>
-          <h2>INTERNSHIP</h2>
-          <p>My primary goal is to find an environment where I can enhance my skills as a developer and make meaningful contributions. I am dedicated to learning and growing throughout this experience.</p>
+          <h3>INTERNSHIP</h3>
+          <p>My primary goal is to find an environment where I can <span className={styles['bolder']}>enhance my skills</span> as a developer and <span className={styles['underline']}>make meaningful contributions</span>. I am dedicated to <i>learning</i> and <i>growing</i> throughout this experience.</p>
           <p><strong>Dates:</strong></p>
           <p>2024-09-09 - 2024-11-15 Weeks: 10</p>
           <p>2024-12-30 - 2025-04-17 Weeks: 16</p>
         </div>
       </div>
-      </> 
-  )
-}
+      <Footer />
+    </>
+  );
+};
 
-export default ContactPage
+export default ContactPage;
