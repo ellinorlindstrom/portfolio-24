445b22f84d3ddcbdcbee51d20142d72c215402c6
lindstrom.elle@gmail.com <lindstrom.elle@gmail.com>
2025-03-04 17:15:33 +0100

    refine contact page

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -4,6 +4,7 @@ import '../scss/ContactPage.scss';
 const ContactPage: React.FC = () => {
   return (
     <>
+    <div className='contact-page-container'>
       <div className='contact-page-text'>
         <h2 className='contact-page-title'>
           CONTACT
@@ -17,26 +18,27 @@ const ContactPage: React.FC = () => {
       <div className='contact-content'>
         <div className='contact-details-container'>
           <div className='contact-details'>
-            <span className='contact-title'>EMAIL:</span>
+            <span className='contact-title'>EMAIL</span>
             <a href="mailto:lindstrom.elle@gmail.com" className='contact-address'>
              lindstrom.elle@gmail.com
             </a>
           </div>
           <div className='contact-details'>
-            <span className='contact-title'>LINKEDIN:</span>
+            <span className='contact-title'>LINKEDIN</span>
             <a href="https://www.linkedin.com/in/ellinor-amanda-lindstrom/" className='contact-address'>
               Ellinor Lindström
             </a>
           </div>
           <div className='contact-details'>
-            <span className='contact-title'>GITHUB:</span>
+            <span className='contact-title'>GITHUB</span>
             <a href="https://github.com/ellinorlindstrom" className='contact-address'>
               ellinorlindstrom
             </a>
           </div>
         </div>
       </div>
-      <Footer />
+    </div>
+    <Footer />
     </>
   );
 };
