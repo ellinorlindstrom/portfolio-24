7e244c4a9ddf25f4211fd82ae8d91d38c10e2b3a
lindstrom.elle@gmail.com <lindstrom.elle@gmail.com>
2025-03-04 15:13:10 +0100

    adjust about page with only one mediaquery

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -7,7 +7,7 @@ function AboutPage() {
     <h2 className='about-page-title'>
         ABOUT</h2>
         <p className='about-page-subtitle'>
-        I’m a <span className='italic'>curious</span> and <span className='bold'>enthusiastic</span> developer with a passion for  immersive, and captivating experiences.💫 <br />
+        I’m a <span className='italic'>curious</span> and <span className='bold'>enthusiastic</span> developer with an intereset in UX, motion- and interactive designs.💫 <br />
         I am currently an intern at <a href='https://coolstuff.se' target='_blank' rel='noopener noreferrer' className='link-no-underline'>Coolstuff</a>, learning about the ins and outs of web development. For inquiries, contact me <Link className='animatedUnderline' to='/contact'>here:)</Link> 
   </p>
         </div>
